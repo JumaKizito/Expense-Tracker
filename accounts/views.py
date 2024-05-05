741f40cb2bd4f930856e07bd72c2690d67820b9c
munuhee <murichustephen@gmail.com>
2024-05-05 12:59:12 +0300

    Feat: Add authentication

---
+++ accounts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
+            return redirect('login')  # Redirect to login page after successful registration
+        else:
+            # If form is not valid, display error messages
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'{field}: {error}')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def logout_view(request):
+    return render(request, 'registration/logout.html')
--- core/views.py
+++ core/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.db import models
 from .models import Department, Project, Category, Budget_Management, Expense_Management
 from .forms import DepartmentForm, ProjectForm, CategoryForm, BudgetManagementForm, ExpenseManagementForm
 
 # Create your views here.
+@login_required
 def dashboard_view(request):
     # Retrieve data for the dashboard
     budget_data = Budget_Management.objects.all()
     expense_data = Expense_Management.objects.all()
     departments = Department.objects.count()
     projects = Project.objects.count()
-    
+
     # Calculate total budget and total expenses
     total_budget = budget_data.aggregate(total_budget=models.Sum('amount'))['total_budget']
     total_expenses = expense_data.aggregate(total_expenses=models.Sum('amount'))['total_expenses']
 
-    
+
     # Calculate remaining budget
     remaining_budget = total_budget - total_expenses if total_budget is not None and total_expenses is not None else None
-    
+
     # Prepare the context dictionary
     context = {
         'total_budget': total_budget,
@@ -28,21 +30,24 @@ def dashboard_view(request):
         'projects': projects,
         # Pass any other data you want to display on the dashboard
     }
-    
+
     # Render the dashboard template with the data
     return render(request, 'core/dashboard.html', context)
 
+@login_required
 def expense_list(request):
     """List expense"""
     expenses = Expense_Management.objects.all()
     context = {'expenses': expenses}
     return render(request, 'core/expense_list.html', context)
 
+@login_required
 def expense_detail(request, pk):
     expense = get_object_or_404(Expense_Management, pk=pk)
     context = {'expense': expense}
     return render(request, 'core/expense_detail.html', context)
 
+@login_required
 def expense_create(request):
     # Check if the request method is POST
     if request.method == 'POST':
@@ -61,6 +66,7 @@ def expense_create(request):
     return render(request, 'core/expense_form.html', {'form': form})
 
 
+@login_required
 def expense_update(request, pk):
     # Retrieve the expense object with the given primary key or return a 404 error
     expense = get_object_or_404(Expense_Management, pk=pk)
@@ -80,6 +86,7 @@ def expense_update(request, pk):
     # Render the expense update form template with the form instance
     return render(request, 'core/expense_form.html', {'form': form})
 
+@login_required
 def expense_delete(request, pk):
     # Retrieve the expense object with the given primary key or return a 404 error
     expense = get_object_or_404(Expense_Management, pk=pk)
@@ -93,14 +100,17 @@ def expense_delete(request, pk):
     return render(request, 'core/expense_confirm_delete.html', {'expense': expense})
 
 # Department Views
+@login_required
 def department_list(request):
     departments = Department.objects.all()
     return render(request, 'core/department_list.html', {'departments': departments})
 
+@login_required
 def department_detail(request, pk):
     department = get_object_or_404(Department, pk=pk)
     return render(request, 'core/department_detail.html', {'department': department})
 
+@login_required
 def department_create(request):
     if request.method == 'POST':
         form = DepartmentForm(request.POST)
@@ -111,6 +121,7 @@ def department_create(request):
         form = DepartmentForm()
     return render(request, 'core/department_form.html', {'form': form})
 
+@login_required
 def department_update(request, pk):
     department = get_object_or_404(Department, pk=pk)
     if request.method == 'POST':
@@ -122,6 +133,7 @@ def department_update(request, pk):
         form = DepartmentForm(instance=department)
     return render(request, 'core/department_form.html', {'form': form})
 
+@login_required
 def department_delete(request, pk):
     department = get_object_or_404(Department, pk=pk)
     if request.method == 'POST':
@@ -133,10 +145,12 @@ def category_list(request):
     categories = Category.objects.all()
     return render(request, 'core/category_list.html', {'categories': categories})
 
+@login_required
 def category_detail(request, pk):
     category = get_object_or_404(Category, pk=pk)
     return render(request, 'core/category_detail.html', {'category': category})
 
+@login_required
 def category_create(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
@@ -147,6 +161,7 @@ def category_create(request):
         form = CategoryForm()
     return render(request, 'core/category_form.html', {'form': form})
 
+@login_required
 def category_update(request, pk):
     category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
@@ -158,6 +173,7 @@ def category_update(request, pk):
         form = CategoryForm(instance=category)
     return render(request, 'core/category_form.html', {'form': form})
 
+@login_required
 def category_delete(request, pk):
     category = get_object_or_404(Category, pk=pk)
     if request.method == 'POST':
@@ -165,14 +181,17 @@ def category_delete(request, pk):
         return redirect('category_list')
     return render(request, 'core/category_confirm_delete.html', {'category': category})
 
+@login_required
 def budget_list(request):
     budgets = Budget_Management.objects.all()
     return render(request, 'core/budget_list.html', {'budgets': budgets})
 
+@login_required
 def budget_detail(request, pk):
     budget = get_object_or_404(Budget_Management, pk=pk)
     return render(request, 'core/budget_detail.html', {'budget': budget})
 
+@login_required
 def budget_create(request):
     if request.method == 'POST':
         form = BudgetManagementForm(request.POST)
@@ -183,6 +202,7 @@ def budget_create(request):
         form = BudgetManagementForm()
     return render(request, 'core/budget_create.html', {'form': form})
 
+@login_required
 def budget_update(request, pk):
     budget = get_object_or_404(Budget_Management, pk=pk)
     if request.method == 'POST':
@@ -194,6 +214,7 @@ def budget_update(request, pk):
         form = BudgetManagementForm(instance=budget)
     return render(request, 'core/budget_update.html', {'form': form})
 
+@login_required
 def budget_delete(request, pk):
     budget = get_object_or_404(Budget_Management, pk=pk)
     budget.delete()
