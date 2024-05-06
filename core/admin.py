759a7e82ce48c93c7f34f2ac5a072e7f8cebd00f
munuhee <murichustephen@gmail.com>
2024-05-06 16:25:15 +0300

    incorporate Django's ModelAdmin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
-from .models import Budget_Management, Category, Expense_Management, Project, Department
+from .models import Department, Project, Category, Budget_Management, Expense_Management
 
-# Register your models here.
-admin.site.register(Budget_Management)
-admin.site.register(Category)
-admin.site.register(Expense_Management)
-admin.site.register(Project)
-admin.site.register(Department)
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'date_created')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'description')
+
+@admin.register(Budget_Management)
+class BudgetManagementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'project', 'amount', 'category', 'date_created')
+    list_filter = ('department', 'project', 'category')
+    search_fields = ('name', 'remarks')
+
+@admin.register(Expense_Management)
+class ExpenseManagementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'department', 'project', 'amount', 'date_created')
+    list_filter = ('department', 'project')
+    search_fields = ('name', 'remarks')
